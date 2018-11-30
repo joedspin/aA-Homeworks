598145c04230011c256e1655c9d5115c2098ea03
Joe D'Espinosa <jtdespinosa@gmail.com>
2018-11-29 22:15:27 -0500

    Define Map class

--- W1D5/abstract_data_types.rb
+++ W1D5/abstract_data_types.rb
@@ -40,5 +40,53 @@ class Stack
         #returns, but doesn't remove, the bottom element in the queue
         @ivar.first
     end
+  end
 
+# Define a Map class with the following instance methods:
+# set(key, value), get(key), delete(key), show
+
+class Map
+    def initialize
+      # create ivar to store stack here!
+      @ivar = Array.new { Array.new(2) }
+    end
+
+    def set(key, value)
+      # adds an element to the map
+      check_key = key_position(key)
+      if check_key.nil?
+        @ivar << [key,value]
+      else
+        @ivar[check_key,1] = value
+      end
+    end
+
+    def key_position(key)
+      @ivar.each_with_index { |el, i| return i if el[0] == key}
+      nil
+    end
+
+    def get(key)
+      # return the value stored at the key location
+      check_key = key_position(key)
+      if check_key.nil?
+        nil
+      else
+        @ivar[check_key, 1]
+      end
+    end
+
+    def delete(key)
+      # returns, but doesn't remove, the top element in the stack
+      check_key = key_position(key)
+      unless check_key.nil?
+        @ivar[check_key].delete
+      end
+    end
+
+    def show
+    # why do they tell us they need this method
+    # but not explain what it is supposed to do?
+    p @ivar
+    end
   end
